a91453501c9ec9a3c28877e8f70c07ca8240ef13
inthecatsdreams <inthecatsdreams@yandex.ru>
2020-06-06 19:48:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# PS3 Discord Rich Presence
+Adds Discord rich presence to your PS3 without relying on PSN.
+
+
+# Requirements
+
+- webMAN installed on your PS3
+- python3 and pip3 on your computer
+
+**Dependencies**
+
+- bs4
+- python3-requests
+
+
+install them like so:
+
+`pip3 install bs4 requests --users`
---
+++ ps3.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+from pypresence import Presence
+import time
+
+def connect_to_console(console_ip):
+    url = "http://{}/cpursx.ps3?/sman.ps3".format(console_ip)
+    connected = False
+    try:
+        html = requests.get(url).text
+        connected = True
+        return connected
+    except TimeoutError:
+        return connected
+
+
+def get_console_info(console_ip):
+    url = "http://{}/cpursx.ps3?/sman.ps3".format(console_ip)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    strings = soup.findAll('a', attrs={'class': 's'})
+    return strings
+
+def get_game(console_ip):
+    url = "http://{}/cpursx.ps3?/sman.ps3".format(console_ip)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    strings = soup.findAll('font', attrs={'size': 3})
+    if strings == None:
+        game = "Nothing"
+        return game
+    else:
+        try:
+            game = strings[-1].text
+        except IndexError:
+            game = "Nothing"
+    
+    return game
+    
+
+
+def get_temps(div):
+    temps = div.text.replace("Â", "")
+    return temps
+
+def get_firmware(div):
+    crap = div.text
+    formatted_fw = ""
+    for l in crap:
+        if l != "P":
+            formatted_fw += l
+        else:
+            break
+    return formatted_fw
+
+
+print("Welcome to PS3 Rich presence by in the cat's dreams\n")
+ps3_ip = input("Please enter your PS3 local IP address:")
+
+if connect_to_console(ps3_ip):
+    print("Connection established with your console.")
+    print(get_game(ps3_ip))
+    client_id = '718828414525505588'  
+    RPC = Presence(client_id)
+    RPC.connect()
+
+    while True:  
+        ps3_info = get_console_info(ps3_ip)
+        temps = get_temps(ps3_info[0])
+        fw = get_firmware(ps3_info[4])
+        game = get_game(ps3_ip)
+        status = "🎮: {}".format(game)
+        RPC.update(large_image="logo", large_text=status, small_image="logo", small_text=status, details="CFW: {}".format(fw), state= status)
+        time.sleep(15) 
+
+else:
+    print("Something went wrong, aborting...")
+    exit()
